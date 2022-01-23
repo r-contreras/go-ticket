c37f7f050066d96918bdf448d54e70525013704f
r-contreras <rcontreras1905@gmail.com>
2022-01-22 20:35:12 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+**Go Ticket**
+
+Web application to buy tickets for movies written in Go and React.
---
+++ frontend/go-ticket/src/App.js
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1 className='text-3xl font-bold text-center' > This is the initial commit of this project :)</h1>
+  );
+}
+
+export default App;
